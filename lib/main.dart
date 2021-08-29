ad94164b522c9a8d18e20010987fbd7ca1f58a8f
kennyobey <kennyobey@gmail.com>
2021-08-29 16:39:07 +0100

    DSC WEEK FIVE WITH ERROR

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-     
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -23,36 +22,60 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  Future getPost()async{
-      var response = await http.get(Uri.https('jsonplaceholder.typicode.com', 'posts'));
-      var jsonData = jsonDecode(response.body);
-      List<Post> posts = [];
-      for (var p in jsonData){
-        Post post = Post (p['title'], p['body']);
-        posts.add(post);
-      }
-      print(posts.length);
-      return posts;
+  Future getPost() async {
+    var response =
+        await http.get(Uri.https('jsonplaceholder.typicode.com', 'posts'));
+    var jsonData = jsonDecode(response.body);
+    List<Post> posts = [];
+    for (var p in jsonData) {
+      Post post = Post(p['title'], p['body']);
+      posts.add(post);
+    }
+    print(posts.length);
+    return posts;
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Post result"),),
-        body: Center(child: ElevatedButton(onPressed: () {getPost(); },
-        child: Text("Check Post"),),),
+        title: Text("Post result"),
+      ),
+      body: Container(
+        child: Card(
+          child: FutureBuilder<List<Post>>(
+            future: getPost(),
+            builder: (context, snapshot) {
+              if (snapshot.data == null) {
+                return Container(
+                  child: Center(
+                    child: Text('Loading...'),
+                  ),
+                );
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, i) {
+                      return ListTile(
+                        title: Text(snapshot.data![i].title),
+                        subtitle: Text(snapshot.data![i].body),
+                      );
+                    });
+              }
+            },
+          ),
+        ),
+      ),
     );
   }
 }
 
-class Post{
+class Post {
   final String title, body;
 
   Post(this.title, this.body);
